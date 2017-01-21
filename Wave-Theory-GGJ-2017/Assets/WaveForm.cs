ffb6d2489e839400f05c081750654914682a1afe
Rienzi Gokea <RGokea@gmail.com>
2017-01-21 17:04:53 -0500

    Finished Square Wave

--- Wave-Theory-GGJ-2017/Assets/WaveForm.cs
+++ Wave-Theory-GGJ-2017/Assets/WaveForm.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class WaveForm : MonoBehaviour {
 
 	public const int numPoints = 40;
-
-	public float singleFrequency = 1f;
-	public float singleAmplitude = 1f;
+	public const float maxAmplitude = 1f;
 
 
 	public GameObject wavePoint;
+
+	public int type = 0;
+
+	public float singleFrequency = 5f;
+
+
 
 	private GameObject[] pointPositions;
 	private float[] radiusWavesTime;
@@ -32,7 +36,7 @@ public class WaveForm : MonoBehaviour {
 			pointPositions[i] = point;
 			radiusWavesTime[i] = (1 / (float)numPoints) * i;
 			frequency[i] = singleFrequency;
-			amplitude[i] = 0;//singleAmplitude;
+			amplitude[i] = 0;
 		}
 
 
@@ -52,8 +56,22 @@ public class WaveForm : MonoBehaviour {
 			if (i > 0 && (amplitude[i] > amplitude[i-1]))
 				amplitude[i] = (amplitude[i] + amplitude[i - 1]) / 2f;
 
+			if (type == 0)
+				pointPositions[i].transform.localPosition = new Vector3(0, amplitude[i] * Mathf.Sin(radiusWavesTime[i] * 2 * Mathf.PI), pointPositions[i].transform.localPosition.z);
+			else if (type == 1) {  // Square Wave
+				float ypos = 0;
+				if (Mathf.Sin(radiusWavesTime[i] * 2 * Mathf.PI) > 0f)
+					ypos = 1;
+				else if (Mathf.Sin(radiusWavesTime[i] * 2 * Mathf.PI) <= 0f)
+					ypos = -1;
 
-			pointPositions[i].transform.localPosition = new Vector3(0, 1 + amplitude[i] * Mathf.Sin(radiusWavesTime[i] * 2 * Mathf.PI), pointPositions[i].transform.localPosition.z);
+				pointPositions[i].transform.localPosition = new Vector3(0, amplitude[i] * ypos, pointPositions[i].transform.localPosition.z);
+			}
+			else if (type == 2) {
+				
+			
+			}
+
 		}
 
 
@@ -66,7 +84,7 @@ public class WaveForm : MonoBehaviour {
 
 
 		if (Input.GetKey(KeyCode.Space)) {
-			amplitude[0] = 1;
+			amplitude[0] = maxAmplitude;
 		}
 		else { 
 			StopCoroutine("StartWave");
@@ -88,10 +106,16 @@ public class WaveForm : MonoBehaviour {
 		}
 		for (int i = 1; i < numPoints; i++) {
 			radiusWavesTime[i] = i / numPoints;
-			amplitude[i] = 1;
+			amplitude[i] = maxAmplitude;
 			yield return new WaitForSeconds(1f / numPoints);
 			//yield return null;
 		}
 		startingWave = false;
+	}
+
+
+	private float SawWave(float t) {
+		//if ()
+
 	}
 }
